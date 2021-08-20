4d25c55383b84fc8b14377834cbf8bda5d5fb9f6
Kapilan M <mnckapilan@gmail.com>
2021-08-20 05:05:25 -0400

    TA2-16: Adds JavaStreamingContext and SparkSession

--- src/main/java/com/cs/rfq/decorator/RfqDecoratorMain.java
+++ src/main/java/com/cs/rfq/decorator/RfqDecoratorMain.java
@@ -11,13 +11,16 @@ public class RfqDecoratorMain {
         System.setProperty("hadoop.home.dir", "C:\\Java\\hadoop-2.9.2");
         System.setProperty("spark.master", "local[4]");
 
-        //TODO: create a Spark configuration and set a sensible app name
+        SparkConf conf = new SparkConf().setAppName("StreamFromSocket");
 
-        //TODO: create a Spark streaming context
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
 
-        //TODO: create a Spark session
+        SparkSession session = SparkSession
+                .builder()
+                .appName("Dataset with SQL")
+                .getOrCreate();
 
-        //TODO: create a new RfqProcessor and set it listening for incoming RFQs
+        RfqProcessor rfqProcessor = new RfqProcessor(session, jssc);
+        rfqProcessor.startSocketListener();
     }
-
 }
